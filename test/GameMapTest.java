3cb7bb85aaf8a420f761558d5ea21fe42e79a528
rao xunrong <raoxunrong@gmail.com>
2013-02-20 13:52:23 +0800

    Add unit test for GameMap, verify the displayname of each ground

--- test/GameMapTest.java
+++ test/GameMapTest.java
@@ -26,6 +26,23 @@ public class GameMapTest {
 
         assertThat(grounds.size(), is(70));
         assertThat(grounds.get(0), isGroundAs("S"));
+        assertThat(grounds.get(1), isGroundAs("0"));
+        assertThat(grounds.get(13), isGroundAs("0"));
+        assertThat(grounds.get(14), isGroundAs("H"));
+        assertThat(grounds.get(15), isGroundAs("0"));
+        assertThat(grounds.get(27), isGroundAs("0"));
+        assertThat(grounds.get(28), isGroundAs("T"));
+        assertThat(grounds.get(29), isGroundAs("0"));
+        assertThat(grounds.get(34), isGroundAs("0"));
+        assertThat(grounds.get(35), isGroundAs("G"));
+        assertThat(grounds.get(36), isGroundAs("0"));
+        assertThat(grounds.get(48), isGroundAs("0"));
+        assertThat(grounds.get(49), isGroundAs("P"));
+        assertThat(grounds.get(50), isGroundAs("0"));
+        assertThat(grounds.get(62), isGroundAs("0"));
+        assertThat(grounds.get(63), isGroundAs("M"));
+        assertThat(grounds.get(64), isGroundAs("$"));
+        assertThat(grounds.get(69), isGroundAs("$"));
     }
 
     private Matcher<? super Ground> isGroundAs(final String s) {
@@ -37,7 +54,6 @@ public class GameMapTest {
 
             @Override
             public void describeTo(Description description) {
-                //To change body of implemented methods use File | Settings | File Templates.
             }
         };
     }
