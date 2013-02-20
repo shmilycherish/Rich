94decb9e68762ae80f90b1aff8ed016224fe54cc
rao xunrong <raoxunrong@gmail.com>
2013-02-20 14:14:04 +0800

    Modify unit test for GameMap, verify the point

--- test/GameMapTest.java
+++ test/GameMapTest.java
@@ -24,31 +24,31 @@ public class GameMapTest {
         List<Ground> grounds = gameMap.initalizeMap();
 
         assertThat(grounds.size(), is(70));
-        assertThat(grounds.get(0), isGroundAs("S", 0));
-        assertThat(grounds.get(1), isGroundAs("0", 200));
-        assertThat(grounds.get(13), isGroundAs("0", 200));
-        assertThat(grounds.get(14), isGroundAs("H", 0));
-        assertThat(grounds.get(15), isGroundAs("0", 200));
-        assertThat(grounds.get(27), isGroundAs("0", 200));
-        assertThat(grounds.get(28), isGroundAs("T", 0));
-        assertThat(grounds.get(29), isGroundAs("0", 500));
-        assertThat(grounds.get(34), isGroundAs("0", 500));
-        assertThat(grounds.get(35), isGroundAs("G", 0));
-        assertThat(grounds.get(36), isGroundAs("0", 300));
-        assertThat(grounds.get(48), isGroundAs("0", 300));
-        assertThat(grounds.get(49), isGroundAs("P", 0));
-        assertThat(grounds.get(50), isGroundAs("0", 300));
-        assertThat(grounds.get(62), isGroundAs("0", 300));
-        assertThat(grounds.get(63), isGroundAs("M", 0));
-        assertThat(grounds.get(64), isGroundAs("$", 0));
-        assertThat(grounds.get(69), isGroundAs("$", 0));
+        assertThat(grounds.get(0), isGroundAs("S", 0, 0));
+        assertThat(grounds.get(1), isGroundAs("0", 200, 0));
+        assertThat(grounds.get(13), isGroundAs("0", 200, 0));
+        assertThat(grounds.get(14), isGroundAs("H", 0, 0));
+        assertThat(grounds.get(15), isGroundAs("0", 200, 0));
+        assertThat(grounds.get(27), isGroundAs("0", 200, 0));
+        assertThat(grounds.get(28), isGroundAs("T", 0, 0));
+        assertThat(grounds.get(29), isGroundAs("0", 500, 0));
+        assertThat(grounds.get(34), isGroundAs("0", 500, 0));
+        assertThat(grounds.get(35), isGroundAs("G", 0, 0));
+        assertThat(grounds.get(36), isGroundAs("0", 300, 0));
+        assertThat(grounds.get(48), isGroundAs("0", 300, 0));
+        assertThat(grounds.get(49), isGroundAs("P", 0, 0));
+        assertThat(grounds.get(50), isGroundAs("0", 300, 0));
+        assertThat(grounds.get(62), isGroundAs("0", 300, 0));
+        assertThat(grounds.get(63), isGroundAs("M", 0, 0));
+        assertThat(grounds.get(64), isGroundAs("$", 0, 20));
+        assertThat(grounds.get(69), isGroundAs("$", 0, 60));
     }
 
-    private Matcher<? super Ground> isGroundAs(final String s, final int price) {
+    private Matcher<? super Ground> isGroundAs(final String s, final int price, final int point) {
         return new TypeSafeMatcher<Ground>() {
             @Override
             protected boolean matchesSafely(Ground ground) {
-                return ground.getDisplay().equals(s) && (ground.getPrice() == price);
+                return ground.getDisplay().equals(s) && (ground.getPrice() == price) && (ground.getPoint() == point);
             }
 
             @Override
