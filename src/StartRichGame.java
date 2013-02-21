9ba1d9f4d17b085a30d053581dad02570b03eeab
shmilycherish <shmilycherish@gmail.com>
2013-02-21 23:02:58 +0800

    TDD

---
+++ src/StartRichGame.java
@@ -0,0 +1,18 @@
+import java.util.Iterator;
+
+
+public class StartRichGame {
+
+	private int funds;
+
+	public Integer getFunds() {
+		// TODO Auto-generated method stub
+		return funds;
+	}
+
+	public void setFunds(int funds) {
+		// TODO Auto-generated method stub
+		this.funds=funds;
+	}
+
+}
