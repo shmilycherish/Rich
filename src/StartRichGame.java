d410eb16ae0fd14dcfe8e6d44b5af86f2a9783b9
shmilycherish <shmilycherish@gmail.com>
2013-02-22 13:43:27 +0800

    TDD

--- src/StartRichGame.java
+++ src/StartRichGame.java
@@ -1,9 +1,11 @@
+import java.util.HashMap;
 import java.util.Iterator;
 
 
 public class StartRichGame {
 
 	private int funds;
+	private String playersTypes;
 
 	public Integer getFunds() {
 		// TODO Auto-generated method stub
@@ -13,6 +15,27 @@ public class StartRichGame {
 	public void setFunds(int funds) {
 		// TODO Auto-generated method stub
 		this.funds=funds;
+	}
+
+	public void setPlayersTypes(String playersTypes) {
+		// TODO Auto-generated method stub
+		this.playersTypes=playersTypes;
+	}
+
+	public String getPlayersTypes() {
+		// TODO Auto-generated method stub
+		return playersTypes;
+	}
+
+	public HashMap InitialPlayers(int funds, String playersTypes) {
+		// TODO Auto-generated method stub		
+		HashMap gamePlayers=new HashMap();				
+		GamePlayer gamePlayer;		
+		for(int i=0;i<playersTypes.length();i++){			
+			gamePlayer=new GamePlayer(playersTypes.substring(i, i+1),funds);			
+			gamePlayers.put(playersTypes.substring(i, i+1), gamePlayer);
+		}
+		return gamePlayers;		
 	}
 
 }
--- test/StartRichGameTest.java
+++ test/StartRichGameTest.java
@@ -1,6 +1,10 @@
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
 import org.junit.Test;
 import org.mockito.Mockito;
 
+import java.util.HashMap;
 import java.util.Iterator;
 
 import static org.hamcrest.core.Is.is;
@@ -22,9 +26,34 @@ public class StartRichGameTest {
     }
 
     @Test
-    public void shouldGet14000WhenUserInputIis14000t(){
+    public void shouldInitialPlayersTypesUsingUserInput(){
         StartRichGame startRichGame=new StartRichGame();
-        startRichGame.getUserInuptFunds();
-        assertThat(startRichGame.getUserInuptFunds(), is(14000));
+        startRichGame.setPlayersTypes("1234");
+        assertThat(startRichGame.getPlayersTypes(), is("1234"));
+    }
+
+    @Test
+    public void shouldInitialPlayersUsingFundsAndPlayersTypes(){
+        StartRichGame startRichGame=new StartRichGame();
+        startRichGame.setFunds(15000);
+        startRichGame.setPlayersTypes("1234");
+        HashMap gamePlayers=startRichGame.InitialPlayers(startRichGame.getFunds(),startRichGame.getPlayersTypes());
+        assertThat((GamePlayer)gamePlayers.get("1"),isGamePlayerAs("Q",15000,"1"));
+        assertThat((GamePlayer)gamePlayers.get("2"),isGamePlayerAs("A",15000,"2"));
+        assertThat((GamePlayer)gamePlayers.get("3"),isGamePlayerAs("S",15000,"3"));
+        assertThat((GamePlayer)gamePlayers.get("4"),isGamePlayerAs("J",15000,"4"));
+    }
+    
+    private Matcher<? super GamePlayer> isGamePlayerAs(final String display, final int funds, final String charactersType) {
+        return new TypeSafeMatcher<GamePlayer>() {
+            @Override
+            protected boolean matchesSafely(GamePlayer gamePlayer) {
+                return gamePlayer.display.equals(display) && (gamePlayer.funds==funds) && gamePlayer.charactersType.equals(charactersType);
+            }
+
+            @Override
+            public void describeTo(Description description) {
+            }
+        };
     }
 }
