f8653d2cf4e684571a31bc4ba68d692acb167c75
shmilycherish <shmilycherish@gmail.com>
2013-02-23 23:34:02 +0800

    TDD

---
+++ src/UserInput.java
@@ -0,0 +1,20 @@
+import java.util.Iterator;
+import java.util.Scanner;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: cherish
+ * Date: 13-2-23
+ * Time: 下午7:34
+ * To change this template use File | Settings | File Templates.
+ */
+public class UserInput {
+    private Scanner scanner=new Scanner(System.in);
+    public String readUserInput() {
+        return    scanner.next();
+    }
+
+    public void printMessage(String message) {
+        System.out.println(message);
+    }
+}
